bfb649de1cf6d3ac502a282ea98b2e2fa19ef686
Ihar Amelyaniuk <ihar_amelyaniuk@epam.com>
2018-06-22 18:33:40 +0300

    added twitter strategy

--- express-server/config/passport.js
+++ express-server/config/passport.js
@@ -1,5 +1,6 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
+import TwitterStrategy from 'passport-twitter';
 import user from '../config/user.json';
 
 const localLogin = new LocalStrategy({
@@ -14,4 +15,16 @@ const localLogin = new LocalStrategy({
   }
 });
 
+const twitterAuth = new TwitterStrategy({
+  consumerKey: 	'MiopzfIjkos4or6NvovL32zjv',
+  consumerSecret: '	7IwseVUO3G5wSUktyPQ5ILAwHjhm018nXXFijSWtqzysVf9q0q',
+  callbackURL: "http://127.0.0.1:8080/api/auth/twitter/callback"
+},function(token, tokenSecret, profile, cb) {
+  User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+  } 
+);
+
 passport.use(localLogin)
+        .use(twitterAuth);
--- express-server/routes/auth.js
+++ express-server/routes/auth.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import passport from 'passport';
 import * as Auth from '../controllers/auth';
 const router = express.Router();
 
 router.post('/', Auth.signin);
+
+
+router.get('/twitter',
+  passport.authenticate('twitter'));
+
+router.get('/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.send('Twitter');
+    // res.redirect('/');
+  });
 
 export default router;
